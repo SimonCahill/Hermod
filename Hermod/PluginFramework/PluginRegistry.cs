8fc3c37ac9d317e1794442f913c072efe0f0d899
Simon Cahill <simon@simonc.eu>
2023-02-21 19:28:54 +0100

    Began implementing PluginRegistry class

---
+++ Hermod/PluginFramework/PluginRegistry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hermod.PluginFramework {
+
+    /// <summary>
+    /// Handles the loading, unloading, and general management of plugins.
+    ///
+    /// This class knows which plugins are loaded at any given time, can all any
+    /// commands provided by the plugin and also fire events.
+    /// </summary>
+    internal sealed class PluginRegistry {
+
+        #region Singleton
+        private PluginRegistry() { }
+
+        private static PluginRegistry? _instance;
+
+        /// <summary>
+        /// Gets the current instance of this object.
+        /// </summary>
+        public static PluginRegistry Instance => _instance ??= new PluginRegistry();
+        #endregion
+
+
+    }
+}
+
