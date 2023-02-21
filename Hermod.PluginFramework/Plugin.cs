00b14a92f7b5266367612c897d7f068206b7fa2a
Simon Cahill <simon@simonc.eu>
2023-02-21 19:30:40 +0100

    Added abstract Plugin class

---
+++ Hermod.PluginFramework/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Hermod.Core.Commands;
+using Serilog;
+
+namespace Hermod.PluginFramework {
+
+    /// <summary>
+    /// An abstract class for plugins with all the main features already implemented.
+    /// </summary>
+    public abstract class Plugin: IPlugin {
+
+		/// <summary>
+		/// Specialised constructor; allows inheriting classes to set their values immediately.
+		/// </summary>
+		/// <param name="pluginName">The name of the plugin.</param>
+		/// <param name="pluginVersion">The plugin's version.</param>
+		/// <param name="commands">A list of commands (if any).</param>
+		public Plugin(string pluginName, Version pluginVersion, params ICommand[] commands) {
+			PluginVersion = pluginVersion;
+			PluginName = pluginName;
+			PluginCommands = commands;
+		}
+
+		/// <inheritdoc/>
+		public Version PluginVersion { get; protected set; }
+
+		/// <inheritdoc/>
+		public string PluginName { get; protected set; }
+
+		/// <inheritdoc/>
+		public ICommand[] PluginCommands { get; protected set; }
+
+		/// <inheritdoc/>
+		public abstract void OnLoad(ILogger logger);
+
+		/// <inheritdoc/>
+		public abstract void OnStart();
+
+		/// <inheritdoc/>
+		public abstract void OnStop();
+	}
+}
+
