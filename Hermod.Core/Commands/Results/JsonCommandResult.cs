df6c4e0866247db75a8551600071dd7727149aa3
Simon Cahill <simon@simonc.eu>
2023-02-21 19:30:21 +0100

    Implemented CommandResult structures

---
+++ Hermod.Core/Commands/Results/JsonCommandResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Hermod.Core.Commands.Results {
+
+    using Newtonsoft.Json;
+
+    public class JsonCommandResult<T>: ICommandResult<T> {
+
+		#region ICommandResult
+        /// <inheritdoc/>
+        public string? Message { get; set; }
+
+        /// <inheritdoc/>
+        public T Result { get; set; }
+		#endregion
+
+		public JsonCommandResult() { }
+
+        public JsonCommandResult(string message, T result) {
+            Message = message;
+            Result = result;
+        }
+
+        /// <summary>
+        /// Converts the <see cref="Result"/> to a JSON-serialised string.
+        /// </summary>
+        /// <param name="settings">Settings for the JSON serialiser.</param>
+        /// <returns>A string representation of <see cref="Result"/></returns>
+        public string? ToJson(JsonSerializerSettings? settings) => JsonConvert.SerializeObject(Result, settings);
+
+        /// <summary>
+        /// Converts the <see cref="Result"/> to a JSON-serialised string.
+        /// </summary>
+        /// <param name="formatting">Formatting settings for the JSON serialiser.</param>
+        /// <returns>A string representation of <see cref="Result"/></returns>
+        public string? ToJson(Formatting formatting) => JsonConvert.SerializeObject(Result, formatting);
+    }
+}
+
