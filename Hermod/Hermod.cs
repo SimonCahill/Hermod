4a795241f0fe8d7651e5c37c6a67528b6f1760ab
Simon Cahill <simon@simonc.eu>
2023-02-22 14:37:10 +0100

    Outputting to console is now thread safe. Plugins can now be loaded at startup. Commands can now be executed from an interactive session.

--- Hermod/Hermod.cs
+++ Hermod/Hermod.cs
@@ -4,6 +4,8 @@ namespace Hermod {
 
     using Config;
     using Core;
+    using Core.Commands.Results;
+    using PluginFramework;
 
     using Serilog;
 
@@ -24,6 +26,7 @@ namespace Hermod {
         /// <param name="configManager">The application-wide config instance.</param>
         /// <param name="logger">The application logger.</param>
         public Hermod(ConfigManager configManager, ILogger logger) {
+            m_consoleLock = new object();
             m_configManager = configManager;
             m_appLogger = logger;
             m_keepAlive = true;
@@ -35,8 +38,30 @@ namespace Hermod {
             SetTerminalTitle();
             m_appLogger.Information("Setting up OS event handlers...");
 			Console.CancelKeyPress += Console_CancelKeyPress;
+            try {
+                Console.BufferHeight = 100;
+                Console.BufferWidth = 120;
+            } catch (PlatformNotSupportedException ex) {
+                m_appLogger.Error("Failed to set terminal size!");
+                m_appLogger.Error($"Error: { ex.Message }");
+            }
+
+            m_appLogger.Debug("Setting up PluginRegistry...");
+            PluginRegistry.Instance.AppLogger = m_appLogger;
 
             m_appLogger.Information("Loading plugins...");
+            m_appLogger.Information($"Plugin dir: { m_configManager.GetPluginInstallDir() }");
+            foreach (var plugin in m_configManager.GetPluginInstallDir().EnumerateFiles("*.dll")) {
+                m_appLogger.Information($"Attempting to load { plugin.FullName }...");
+                try {
+                    PluginRegistry.Instance.LoadPlugin(plugin);
+                } catch (Exception ex) {
+                    m_appLogger.Error($"Failed to load assembly { plugin.FullName }!");
+                    m_appLogger.Error($"Error: { ex.Message }");
+
+                    m_appLogger.Debug(ex.StackTrace);
+                }
+            }
         }
 
         /// <summary>
@@ -51,7 +76,25 @@ namespace Hermod {
                     var promptInput = await ShowPrompt();
                     if (string.IsNullOrEmpty(promptInput)) { continue; }
 
+                    var splitString = promptInput.Split(' ', '\t');
+                    if (TryGetCommand(splitString.First(), out var command)) {
+                        if (command is null) { continue; }
 
+                        var argArray = new string[splitString.Length - 1];
+                        if (argArray.Length > 0) {
+                            Array.Copy(splitString[1..], argArray, argArray.Length);
+                        }
+
+                        var result = await command.ExecuteAsync(argArray);
+
+                        if (result is CommandErrorResult && !string.IsNullOrEmpty(result?.Message)) {
+                            ConsoleErrorWrite(result.Message);
+                        } else if (!string.IsNullOrEmpty(result?.Message)) {
+                            ConsoleWrite(result.Message);
+                        }
+                    } else {
+                        m_appLogger.Error($"Command \"{splitString[0] }\" not found!");
+                    }
                 } else {
                     Thread.Sleep(50);
                 }
@@ -100,13 +143,36 @@ namespace Hermod {
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-		private async void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e) {
+		private void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e) {
+            Console.WriteLine(); // make sure the output isn't on the same line as the prompt or output
             m_appLogger.Warning("Received signal SIGNIT (CTRL+C)!");
 			e.Cancel = true;
+
+            if (InteractiveMode) {
+                m_appLogger.Warning("Hermod is running in interactive mode! Please use \"quit\" command!");
+                ShowPrompt();
+                return;
+            }
 
             m_inputCancellationToken.Cancel();
             m_keepAlive = false;
 		}
+
+        private void ConsoleWrite(string message) {
+            lock (m_consoleLock) {
+                Console.WriteLine(message);
+            }
+        }
+
+        private void ConsoleErrorWrite(string message) {
+            lock (m_consoleLock) {
+                var prevBackground = Console.BackgroundColor;
+                var prevForegound = Console.ForegroundColor;
+                Console.Error.WriteLine(message);
+                Console.BackgroundColor = prevBackground;
+                Console.ForegroundColor = prevForegound;
+            }
+        }
     }
 }
 
