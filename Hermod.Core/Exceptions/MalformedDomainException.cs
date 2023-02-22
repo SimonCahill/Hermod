0f779e3ecfc130db0e3d4945f345dee79a1260e7
Simon Cahill <simon@simonc.eu>
2023-02-22 23:10:58 +0100

    Added a new exception that is thrown when a malformed domain name was encountered.

---
+++ Hermod.Core/Exceptions/MalformedDomainException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hermod.Core.Exceptions {
+
+    /// <summary>
+    /// Exception class that is thrown when a malformed FQDN (fully-qualified domain name) was passed somewhere.
+    /// </summary>
+    public class MalformedDomainException: Exception {
+
+        /// <summary>
+        /// Constructs a new instance of this class.
+        /// </summary>
+        /// <param name="fqdn">The offending string that should've been an FQDN.</param>
+        public MalformedDomainException(string fqdn): base("Encountered malformed FQDN!") {
+            Fqdn = fqdn;
+        }
+
+        /// <summary>
+        /// The offending string.
+        /// </summary>
+        public string Fqdn { get; }
+    }
+}
+
