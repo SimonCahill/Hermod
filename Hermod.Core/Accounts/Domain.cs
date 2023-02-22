6e3e51481856a962a6165fa21eda82f613adace0
Simon Cahill <simon@simonc.eu>
2023-02-22 23:10:30 +0100

    Added core classes for domains and domain users.

---
+++ Hermod.Core/Accounts/Domain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Hermod.Core.Accounts {
+
+    using Exceptions;
+
+    /// <summary>
+    /// Represents a single domain.
+    ///
+    /// Each domain can have N <see cref="DomainUser"/> objects associated with it.
+    /// This object can be deserialised directly from a JSON file or database entry.
+    ///
+    /// Each domain object splits the domain into its TLD and the domain name itself.
+    ///
+    /// This is used both while storing domains and accounts, and while storing and indexing emails.
+    /// </summary>
+    public sealed class Domain {
+
+        /// <summary>
+        /// Creates a new instance of this class.
+        /// </summary>
+        /// <param name="id">The domain's ID.</param>
+        /// <param name="tld">The domain's TLD.</param>
+        /// <param name="domainName">The domain name.</param>
+        /// <param name="users">An initial list of <see cref="DomainUser"/> accounts.</param>
+        /// <exception cref="MalformedDomainException">If a malformed FQDN was encountered.</exception>
+        public Domain(int id, string tld, string domainName, params DomainUser[] users) {
+            if (string.IsNullOrEmpty(tld) || string.IsNullOrEmpty(domainName)) { throw new MalformedDomainException($"{ domainName }.{ tld }"); }
+
+            Id = id;
+            Tld = tld;
+            DomainName = domainName;
+            DomainUsers = users.ToList();
+        }
+
+        /// <summary>
+        /// Constructs a new instance of this class.
+        /// </summary>
+        /// <param name="id">The ID for the domain.</param>
+        /// <param name="fqdn">The fully-qualified domain name.</param>
+        /// <param name="users">An initial list of <see cref="DomainUser"/> accounts.</param>
+        /// <exception cref="MalformedDomainException">If a malformed FQDN was encountered.</exception>
+        public Domain(int id, string fqdn, params DomainUser[] users) {
+            if (string.IsNullOrEmpty(fqdn) || !fqdn.Contains('.')) { throw new MalformedDomainException(fqdn); }
+
+            Id = id;
+            var splits = fqdn.Split('.').Where(x => x != ".").ToArray();
+            Tld = splits[0];
+            DomainName = string.Join('.', splits[1..]);
+            DomainUsers = users.ToList();
+        }
+
+        /// <summary>
+        /// The domain's ID.
+        ///
+        /// This is typically a number starting with zero.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The top-level domain.
+        /// </summary>
+        /// <example >
+        /// com
+        /// </example>
+        public string Tld { get; set; }
+
+        /// <summary>
+        /// The name of the domain.
+        /// </summary>
+        /// <example >
+        /// example
+        /// </example>
+        public string DomainName { get; set; }
+
+        public List<DomainUser> DomainUsers { get; }
+
+    }
+}
+
---
+++ Hermod.Core/Accounts/DomainUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Hermod.Core.Accounts {
+
+    /// <summary>
+    /// This class represents a single user (email account) in a domain.
+    ///
+    /// Each <see cref="Domain"/> may contain N <see cref="DomainUser"/> objects.
+    /// This object is tightly tied to the operation of Hermod.
+    /// </summary>
+    public sealed class DomainUser {
+
+        public DomainUser(int id, string accountName, string encryptedPassword) {
+            Id = id;
+            AccountName = accountName;
+            EncryptedPassword = encryptedPassword;
+        }
+
+        /// <summary>
+        /// The domain user's ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The account name (i.e. the login name)
+        /// </summary>
+        public string AccountName { get; set; }
+
+        /// <summary>
+        /// The encrypted user password.
+        /// Hermod does not and will never support using unencrypted passwords!
+        /// </summary>
+        public string EncryptedPassword { get; set; }
+
+    }
+}
+
