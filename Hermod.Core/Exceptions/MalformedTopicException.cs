67692728f9e716ee89e47009d4a6d09f7bcd3f29
Simon Cahill <simon@simonc.eu>
2023-02-22 18:56:58 +0100

    Added an exception to be handled when a malformed topic was passed

---
+++ Hermod.Core/Exceptions/MalformedTopicException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hermod.Core.Exceptions {
+
+    /// <summary>
+    /// Exception class that is thrown when a topic for IPC is malformed.
+    /// </summary>
+    public class MalformedTopicException: Exception {
+
+        /// <summary>
+        /// Constructs a new instance of this class.
+        /// </summary>
+        /// <param name="topic">The topic that is malformed.</param>
+        /// <param name="message">An accompanying message.</param>
+        public MalformedTopicException(string topic, string? message): base(message) {
+            Topic = topic;
+        }
+
+        /// <summary>
+        /// The malformed topic.
+        /// </summary>
+        public string Topic { get; }
+    }
+}
+
