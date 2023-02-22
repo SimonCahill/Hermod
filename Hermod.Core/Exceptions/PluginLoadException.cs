22627e01d8df213be810d054801c71c400414e82
Simon Cahill <simon@simonc.eu>
2023-02-22 18:57:11 +0100

    Added an exception to handle cases where plugin loading failed.

---
+++ Hermod.Core/Exceptions/PluginLoadException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hermod.Core.Exceptions {
+
+    /// <summary>
+    /// Exception class that is thrown when a plugin failed to load.
+    /// </summary>
+    public class PluginLoadException: Exception {
+        public PluginLoadException(string? pluginName): base("Failed to load plugin!") {
+            PluginName = pluginName;
+        }
+
+        public string? PluginName { get; }
+    }
+}
+
