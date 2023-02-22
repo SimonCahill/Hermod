4c8b51799c6648d5d0d9fcc04547a8822b21f99c
Simon Cahill <simon@simonc.eu>
2023-02-22 14:38:39 +0100

    Added a new event which is fired when the application config is loaded.

---
+++ Hermod.Core/IConfigLoaded.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hermod.Config {
+
+    public delegate void ConfigLoadedEventHandler(object? sender, ConfigLoadedEventArgs e);
+
+    public class ConfigLoadedEventArgs: EventArgs { }
+
+    /// <summary>
+    /// Interface defining a contract which notifies anyone whom it may concern
+    /// when the application configuration has been (re-)loaded.
+    /// </summary>
+    public interface IConfigLoaded {
+
+        event ConfigLoadedEventHandler? ConfigLoaded;
+
+    }
+}
+
