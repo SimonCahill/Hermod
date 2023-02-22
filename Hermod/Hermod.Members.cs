a742ed81f9058ca8699cb374bc1b7f6448007746
Simon Cahill <simon@simonc.eu>
2023-02-22 14:37:46 +0100

    Added members for internal commands and the console mutex

--- Hermod/Hermod.Members.cs
+++ Hermod/Hermod.Members.cs
@@ -5,6 +5,7 @@ using System;
 namespace Hermod {
 
     using Config;
+    using Core.Commands;
 
     using Serilog;
 
@@ -16,6 +17,10 @@ namespace Hermod {
         internal volatile bool m_keepAlive; /// A volatile bool indicating whether or not to keep the application alive.
 
         private CancellationTokenSource m_inputCancellationToken; /// CancellationTokenSource for reading input from the console in interactive mode
+
+        private List<ICommand>? m_commands = null; /// A list of built-in commands.
+
+        private readonly object m_consoleLock;
 
 	}
 }
