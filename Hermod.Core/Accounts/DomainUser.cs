6b80e1570af342185dcd620ccf978816beda66bf
Simon Cahill <simon@simonc.eu>
2023-02-23 18:39:40 +0100

    Updated DomainUser class with AccountType and PasswordSalt properties

--- Hermod.Core/Accounts/DomainUser.cs
+++ Hermod.Core/Accounts/DomainUser.cs
@@ -10,15 +10,28 @@ namespace Hermod.Core.Accounts {
     /// </summary>
     public sealed class DomainUser {
 
-        public DomainUser(int id, string accountName, string encryptedPassword) {
+        /// <summary>
+        /// Creates a new instance of this object.
+        /// </summary>
+        /// <param name="id">The user's ID. Leave to -1 if a new account is being created.</param>
+        /// <param name="accountName">The account name.</param>
+        /// <param name="encryptedPassword">The encrypted account password.</param>
+        /// <param name="passwordSalt">The password salt.</param>
+        /// <param name="accType">The account type.</param>
+        public DomainUser(int id, string accountName, string encryptedPassword, string passwordSalt, AccountType accType = AccountType.Imap) {
             Id = id;
             AccountName = accountName;
             EncryptedPassword = encryptedPassword;
+            AccountType = accType;
+            PasswordSalt = passwordSalt;
         }
 
         /// <summary>
         /// The domain user's ID.
         /// </summary>
+        /// <remarks >
+        /// If this is set to -1, an ID will automagically be generated.
+        /// </remarks>
         public int Id { get; set; }
 
         /// <summary>
@@ -31,6 +44,16 @@ namespace Hermod.Core.Accounts {
         /// Hermod does not and will never support using unencrypted passwords!
         /// </summary>
         public string EncryptedPassword { get; set; }
+
+        /// <summary>
+        /// The <see cref="AccountType"/> this account is configured for use with.
+        /// </summary>
+        public AccountType AccountType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the salt to use for password encryption.
+        /// </summary>
+        public string PasswordSalt { get; set; }
 
     }
 }
