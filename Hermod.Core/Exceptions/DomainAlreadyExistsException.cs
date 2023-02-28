7ed62a1beb513af02a361cd266e11fffa60b74bc
Simon Cahill <simon@simonc.eu>
2023-03-01 00:23:51 +0100

    Added a new exception

---
+++ Hermod.Core/Exceptions/DomainAlreadyExistsException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hermod.Core.Exceptions {
+
+    /// <summary>
+    /// Exception class that is thrown when an attempt is made to add a domain which already exists.
+    /// </summary>
+    public class DomainAlreadyExistsException: Exception {
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="msg">A brief description of the error.</param>
+        public DomainAlreadyExistsException(string msg): base(msg) { }
+    }
+}
+
