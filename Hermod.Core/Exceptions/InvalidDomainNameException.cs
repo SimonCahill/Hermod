7414d5f3ca5989b73b69673c4f81473f828b00cd
Simon Cahill <simon@simonc.eu>
2023-03-01 00:48:21 +0100

    Added new exception

---
+++ Hermod.Core/Exceptions/InvalidDomainNameException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hermod.Core.Exceptions {
+
+    /// <summary>
+    /// Exception class that is thrown when an invalid domain name is encountered.
+    /// </summary>
+    public class InvalidDomainNameException: Exception {
+
+        public InvalidDomainNameException(string msg): base(msg) { }
+
+        public InvalidDomainNameException(string domainName, string msg) : base(msg) {
+            DomainName = domainName;
+        }
+
+        /// <summary>
+        /// The offending domain.
+        /// </summary>
+        public string? DomainName { get; }
+    }
+}
+
