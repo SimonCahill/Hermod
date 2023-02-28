a4e74fc65bab6729a2b5acecb5b33ccd97a813fb
Simon Cahill <simon@simonc.eu>
2023-03-01 00:24:45 +0100

    Added new methods to DatabaseConnector abstract class

--- Hermod.EmailImport/Data/DatabaseConnector.cs
+++ Hermod.EmailImport/Data/DatabaseConnector.cs
@@ -62,7 +62,7 @@ namespace Hermod.EmailImport.Data {
         /// Asynchronously purges all databases.
         /// </summary>
         /// <returns>An awaitable <see cref="Task{Int32}"/> indicating the amount of domains purged.</returns>
-        public abstract Task<int> PurgeDatabases();
+        public abstract Task<int> PurgeDatabasesAsync();
 
         /// <summary>
         /// Asynchronously removes a single user from a given domain.
@@ -70,17 +70,32 @@ namespace Hermod.EmailImport.Data {
         /// <param name="domain">The domain from which to remove the user.</param>
         /// <param name="user">The user account to remove.</param>
         /// <returns>An awaitable <see cref="Task{Boolean}"/> indicating whether or not the operation was successful.</returns>
-        public abstract Task<bool> RemoveUserFromDomain(Domain domain, DomainUser user);
+        public abstract Task<bool> RemoveUserFromDomainAsync(Domain domain, DomainUser user);
 
         /// <summary>
         /// Asynchronously purges all users from a given domain.
         /// </summary>
         /// <param name="domain">The domain to purge users from.</param>
         /// <returns>An awaitable <see cref="Task{Int32}"/> indicating the amount of users purged.</returns>
-        public abstract Task<int> PurgeUsersFromDomain(Domain domain);
+        public abstract Task<int> PurgeUsersFromDomainAsync(Domain domain);
 
         /// <inheritdoc/>
         public abstract void Dispose();
+
+        /// <summary>
+        /// Adds a single domain to the database.
+        /// </summary>
+        /// <param name="domainName">The name of the domain to add; must follow the tld.domain nomenclature!</param>
+        /// <returns>An awaitable <see cref="Task{Domain}"/> containing the newly generated domain.</returns>
+        public abstract Task<Domain> AddDomainAsync(string domainName);
+
+        /// <summary>
+        /// Removes a single domain from the database.
+        /// </summary>
+        /// <param name="domain">The domain to remove.</param>
+        /// <returns><code >true</code> if the domain was removed. <code >false</code> otherwise.</returns>
+        /// <exception cref="Core.Exceptions.DomainAlreadyExistsException" >If the domain already exists.</exception>
+        public abstract Task<bool> RemoveDomainAsync(Domain domain);
     }
 }
 
