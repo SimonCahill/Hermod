fcafad910d4c8371b2acf1a325062760d83246c2
Simon Cahill <simon@simonc.eu>
2023-03-01 13:29:34 +0100

    Began implementing commands in EmailImporter

--- Hermod.EmailImport/EmailImporter.CommandHandling.cs
+++ Hermod.EmailImport/EmailImporter.CommandHandling.cs
@@ -1,18 +1,61 @@
 ﻿using System;
 
 namespace Hermod.EmailImport {
-
+    using System.Text;
     using Core.Commands.Results;
     using Hermod.Core.Accounts;
 
     partial class EmailImporter {
 
         private ICommandResult Handle_GetDomains(params string[] args) {
-            return new CommandErrorResult("This command is not yet implemented. Sorry");
+            var domains = m_dbConnector?.GetDomainsAsync(true, args).GetAwaiter().GetResult();
+
+            if (domains is null || !domains.Any()) { return new CommandErrorResult("No domains found matching criteria!"); }
+
+            var sBuilder = new StringBuilder().AppendLine("Got domain(s):");
+
+            foreach (var domain in domains) {
+                sBuilder.Append($"\t{domain.ToString()}");
+
+                if (domain.DomainUsers.Any()) {
+                    sBuilder.AppendLine(":");
+
+                    foreach (var user in domain.DomainUsers) {
+                        sBuilder.AppendLine($"\t\t{user.AccountName} [{user.AccountType.ToString()}]");
+                    }
+                    sBuilder.AppendLine();
+                }
+            }
+
+            return new CommandResult(sBuilder.ToString(), domains);
         }
 
         private ICommandResult Handle_GetSingleDomain(params string[] args) {
-            return new CommandErrorResult("This command is not yet implemented. Sorry");
+            if (args is null || args.Length == 0) {
+                return new CommandErrorResult("At least one domain must be supplied!");
+            }
+
+            var domains = m_dbConnector?.GetDomainsAsync(true).GetAwaiter().GetResult().Where(d => args.Contains(d.ToString()));
+            if (domains is null || domains.Any()) {
+                return new CommandErrorResult("No domains found matching any of the inputs!");
+            }
+
+            var sBuilder = new StringBuilder().AppendLine("Got domain(s):");
+
+            foreach (var domain in domains) {
+                sBuilder.Append($"\t{ domain.ToString() }");
+
+                if (domain.DomainUsers.Any()) {
+                    sBuilder.AppendLine(":");
+
+                    foreach (var user in domain.DomainUsers) {
+                        sBuilder.AppendLine($"\t\t{ user.AccountName } [{ user.AccountType.ToString() }]");
+                    }
+                    sBuilder.AppendLine();
+                }
+            }
+
+            return new CommandResult(sBuilder.ToString(), domains);
         }
 
         private ICommandResult Handle_AddDomain(params string[] args) {
