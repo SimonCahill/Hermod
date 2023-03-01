e34235be4e6166a1ce513849c8514e17659dcbc5
Simon Cahill <simon@simonc.eu>
2023-03-01 01:54:30 +0100

    Began adding "add-domain" command

--- Hermod.EmailImport/EmailImporter.CommandHandling.cs
+++ Hermod.EmailImport/EmailImporter.CommandHandling.cs
@@ -3,6 +3,7 @@
 namespace Hermod.EmailImport {
 
     using Core.Commands.Results;
+    using Hermod.Core.Accounts;
 
     partial class EmailImporter {
 
@@ -15,7 +16,37 @@ namespace Hermod.EmailImport {
         }
 
         private ICommandResult Handle_AddDomain(params string[] args) {
-            return new CommandErrorResult("This command is not yet implemented. Sorry");
+            if (args.Length == 0) {
+                return new CommandErrorResult("Missing input parameters!");
+            }
+
+            Dictionary<string, string> failedDomains = new Dictionary<string, string>();
+            List<Domain> addedDomains = new List<Domain>();
+
+            foreach (var domain in args) {
+                try {
+                    var newDomain = m_dbConnector?.AddDomainAsync(domain).GetAwaiter().GetResult();
+                    if (newDomain is null) {
+                        throw new Exception($"Failed to add domain { domain }! It was null.");
+                    }
+                    addedDomains.Add(newDomain);
+                } catch (Exception ex) {
+                    failedDomains.Add(domain, ex.Message);
+                }
+            }
+
+            ICommandResult? result;
+
+            if (failedDomains.Count > 0) {
+                result = new CommandErrorResult(
+                    $"Failed to add one or more domains!\n" +
+                    string.Join('\n', failedDomains.Select(x => $"{ x.Key }: Reason: { x.Value }"))
+                );
+            } else {
+                result = new CommandResult($"Added { addedDomains.Count } domains!", addedDomains);
+            }
+
+            return result;
         }
 
         private ICommandResult Handle_RemoveDomain(params string[] args) {
