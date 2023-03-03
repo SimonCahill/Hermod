df24763d9410827a39b7d529158d77648705c33a
Simon Cahill <simon@simonc.eu>
2023-03-03 22:46:16 +0100

    Added new default CommandErrorResult for instances when a command was not yet implemented.

--- Hermod.Core/Commands/Results/CommandErrorResult.cs
+++ Hermod.Core/Commands/Results/CommandErrorResult.cs
@@ -13,6 +13,8 @@ namespace Hermod.Core.Commands.Results {
 		/// <param name="errMsg">The error message.</param>
 		/// <param name="exception">(Optional) Exception that occurred.</param>
 		public CommandErrorResult(string errMsg, Exception? exception = null): base(errMsg, exception) { }
+
+		public static CommandErrorResult GetNotImplementedResult(string cmdName) => new CommandErrorResult($"The command { cmdName } has not yet been implemented.");
 	}
 }
 
